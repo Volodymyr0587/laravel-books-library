7775619bbf73687adddc5d7e8184bc80c3dca511
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-01-08 14:49:36 +0200

    Add Author and Book models with migrations

---
+++ app/Models/Book.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Book extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'author_id',
+        'title',
+        'year_of_publication',
+        'num_of_pages',
+        'genre',
+        'cover',
+    ];
+
+    /**
+     * Get the user that owns the Book
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the author that owns the Book
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+}
---
+++ database/migrations/2025_01_08_122331_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Author;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Author::class)->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->year('year_of_publication');
+            $table->integer('num_of_pages')->nullable();
+            $table->string('genre')->nullable();
+            $table->string('cover')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
